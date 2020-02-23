813e1d74d0bc5f44b58790e9dac3e91c0f4a2ba6
TahaAlTamimi <altamimitmo@gmail.com>
2020-02-23 21:50:10 +0200

    Convert to use Heroku PostgreSQL database

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
     <script src="{{ asset('js/app.js',true) }}" defer></script>
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com" mimeType="font/x-woff">
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com" >
     <link href="https://fonts.googleapis.com/css?family=Nunito"  rel="stylesheet">
     <!-- cdn -->
     <!-- Font Awesome -->
