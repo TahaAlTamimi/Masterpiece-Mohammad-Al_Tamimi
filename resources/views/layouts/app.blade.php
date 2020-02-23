40bafb1914c4dc6d3c3bed6e9fe4fe2eebe2fa4a
TahaAlTamimi <altamimitmo@gmail.com>
2020-02-23 21:46:56 +0200

    Convert to use Heroku PostgreSQL database

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,16 +18,16 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js',true) }}" defer></script>
 
-    <!-- Fonts -->
+    <!-- Fonts
     <link rel="dns-prefetch" href="https://fonts.gstatic.com" mimeType="font/x-woff">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" mimeType="font/x-woff" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito"  rel="stylesheet"> -->
     <!-- cdn -->
     <!-- Font Awesome -->
-<!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"> -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
 <!-- Bootstrap core CSS -->
-<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" mimeType="font/x-woff" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
 <!-- Material Design Bootstrap -->
-<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.10.1/css/mdb.min.css" mimeType="font/x-woff" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.10.1/css/mdb.min.css" rel="stylesheet">
 
 
 <!-- ourdesign -->
